73e2a5a218add2a067690cbf7b57561d930d0953
Shayon Gupta <ShayonX@users.noreply.github.com>
2018-04-01 22:24:40 -0600

    Pulls Trends from Twitter
    Pulls trends from every woeid.

--- KafkaProducer/TwitterTrendProducer.java
+++ KafkaProducer/TwitterTrendProducer.java
@@ -6,22 +6,35 @@ import twitter4j.auth.AccessToken;
 public class TwitterTrendProducer {
 
 
-    public Trends read() throws Exception{
-        AccessToken accessToken = new AccessToken("<Access Token Here>", "<Access Token Secret Here>");
+    public Trends read() throws Exception {
+        AccessToken accessToken = new AccessToken("3161321025-FajWBXoXT4PAZGNyta38w4krEsK8fp2d5opwdN4", "p2BPY6BzUKyQJRmIMDUvAwPXjBCksRTBI12sYi5xdkvrC");
         Twitter twitter = new TwitterFactory().getInstance();
-        twitter.setOAuthConsumer("<Consumer Key (API Key)>", "<Consumer Secret (API Secret)>");
+        twitter.setOAuthConsumer("5VS16zsltlmES0keEq860Xkut", "m6MFR7FTxn2ON3NrMFALD48DFy8ngVxO1KXfE8g1qhaergxpp2");
         twitter.setOAuthAccessToken(accessToken);
         ResponseList<Location> locations;
         locations = twitter.getAvailableTrends();
         System.out.println("Showing available trends");
+        int countWoeid = 0;
+        int array[] = new int[1000];
         for (Location location : locations) {
             System.out.println(location.getName() + " (woeid:" + location.getWoeid() + ")");
+            array[countWoeid] = location.getWoeid();
+            countWoeid++;
         }
-        Trends trends = twitter.getPlaceTrends(2295414);
-        for (int i = 0; i < trends.getTrends().length; i++) {
-            System.out.println(trends.getTrends()[i].getName());
+        int countTrends = 0;
+        Trends trendsRet = twitter.getPlaceTrends(2295414);
+        for (int j = 0; j < 468; j++){
+            Trends trends = twitter.getPlaceTrends(array[j]);
+            for (int i = 0; i < trends.getTrends().length; i++) {
+                System.out.println(trends.getTrends()[i].getName());
+                System.out.println("Current location number out of 467 = "+j);
+                System.out.println("Total number of trends extracted till now = "+countTrends);
+                countTrends++;
         }
-        return trends;
+    }
+        System.out.println("Total number of locations = "+countWoeid);
+        System.out.println("Total number of trends till now = "+countTrends);
+        return trendsRet;
     }
 
     public static void main(String[] args) throws Exception{
