2bd9ab05553659b85131ece7d20734562a3c7eaa
ShivendraAgrawal <shivendra.agrawal@gmail.com>
2018-04-29 17:41:01 -0600

    Add sentiment APIs and modified topic classes

--- tweet_api/application.py
+++ tweet_api/application.py
@@ -39,25 +39,22 @@ def make_base_geojson():
                                                                '3': 'Motorcycle',
                                                                '4': 'Car'},
                                                     'name': 'Sentiment'},
-                                           '5074': {'lookup': {'1': 'Event',
-                                                               '2': 'Sports',
-                                                               '3': 'Politics',
-                                                               '4': 'News',
-                                                               '5': 'Technology',
-                                                               '6': 'Business',
-                                                               '7': 'Entertainment',
-                                                               '8': 'Health'},
+                                           '5074': {'lookup': {1: 'Anger',
+                                                               2: 'Joy',
+                                                               3: 'Neutral',
+                                                               4: 'Sadness'},
                                                     'name': 'Topic'}}},
                  'type': 'FeatureCollection'}
     return base_dict
 
 
 def geoJsonify(raw_json):
-    topic_map = {'Business': '6', 'Entertainment': '7', 'Event': '1', 'Health': '8',
-                'News': '4', 'Politics': '3', 'Sports': '2', 'Technology': '5'}
+    topic_map = {'Entertainment': 1, 'Mood': 2, 'Politics': 3, 'Sports': 4, 'Technology': 5}
+    sentiment_map = {'Anger': 1, 'Joy': 2, 'Neutral': 3, 'Sadness': 4}
 
     Date, Tweet, Topic = raw_json["creation_time"], raw_json["body"], raw_json["topic"]
     Trend, TweetID, User = raw_json["trend"], raw_json["twid"], raw_json["user"]
+    Sentiment = raw_json["sentiment"]
     try:
         location = raw_json["location"].split(",")
     except:
@@ -73,25 +70,28 @@ def geoJsonify(raw_json):
     single_marker['properties']['5058'] = TweetID
     single_marker['properties']['5059'] = User
     if not Topic:
-        Topic = random.choice(['Entertainment', 'Business', 'Health', 'News', 'Sports',
-                               'Event', 'Politics', 'Technology'])
+        Topic = random.choice(['Entertainment', 'Mood', 'Politics', 'Sports', 'Technology'])
     single_marker['properties']['5074'] = topic_map[Topic]
+    if not Sentiment:
+        Sentiment = random.choice(['Anger', 'Joy', 'Neutral', 'Sadness'])
+    single_marker['properties']['5065'] = sentiment_map[Sentiment]
     return single_marker
 
 
 @app.route('/all_markers', methods=['GET','POST'])
 def show_all_markers():
     # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
-    inverse_topic_map = {'1': 'Event', '2': 'Sports', '3': 'Politics',
-                        '4': 'News', '5': 'Technology', '6': 'Business',
-                        '7': 'Entertainment', '8': 'Health'}
+    inverse_topic_map = {1: 'Entertainment', 2: 'Mood',
+                         3: 'Politics', 4: 'Sports', 5: 'Technology'}
+    inverse_sentiment_map = {1: 'Anger', 2: 'Joy', 3: 'Neutral', 4: 'Sadness'}
+
     cluster = Cluster(CASSANDRA_IPS)
     session = cluster.connect('graphy')
     rows = session.execute("SELECT json * FROM tweet limit 100000")
     result = []
-    topic_counts = Counter()
-    trend_class_counter = defaultdict(Counter)
-    trend_classification = {}
+    topic_counts, sentiment_counts = Counter(), Counter()
+    trend_class_counter, trend_sentiment_counter = defaultdict(Counter), defaultdict(Counter)
+    trend_classification, trend_sentiment = {}, {}
 
 
     for i in rows:
@@ -100,17 +100,32 @@ def show_all_markers():
         geo_json = geoJsonify(raw_json)
         if geo_json:
             result.append(geo_json)
+
+            #Counting topics for each trend
             topic = inverse_topic_map[geo_json['properties']['5074']]
             topic_counts[topic] += 1
             trend_class_counter[geo_json['properties']['5057']][topic] += 1
+
+            #Counting sentiment for each trend
+            sentiment = inverse_sentiment_map[geo_json['properties']['5065']]
+            sentiment_counts[sentiment] += 1
+            trend_sentiment_counter[geo_json['properties']['5057']][sentiment] += 1
+
+    #Picking the topic for a trend based on majority topic
     for trend in trend_class_counter:
         trend_classification[trend] = trend_class_counter[trend].most_common()[0][0]
+
+    # Picking the sentiment for a trend based on majority sentiment
+    for trend in trend_sentiment_counter:
+        trend_sentiment[trend] = trend_sentiment_counter[trend].most_common()[0][0]
 
     base_geo_json = make_base_geojson()
     base_geo_json['features'] = result
     return Response(json.dumps({'geo_json' : base_geo_json,
                                 'topic_counts' : topic_counts,
-                                'trend_classification' : trend_classification}),
+                                'trend_classification' : trend_classification,
+                                'sentiment_counts' : sentiment_counts,
+                                'trend_sentiment' : trend_sentiment}),
                     mimetype='application/json')
 
 
